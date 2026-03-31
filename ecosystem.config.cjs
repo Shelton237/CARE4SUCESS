6187b62e42e05c718c293cb8ddf7efdd0241ea2f
eureka <tsagocedric@gmail.com>
2026-03-31 18:36:39 +0100

    fix: update production port to 4002 to match Nginx

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -9,7 +9,7 @@ module.exports = {
             max_memory_restart: "1G",
             env_production: {
                 NODE_ENV: "production",
-                API_PORT: 4000
+                API_PORT: 4002
             }
         }
     ]
